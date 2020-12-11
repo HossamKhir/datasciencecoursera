7aca70506135fcb3f6fe0aace0b346bc6b7a2984
Khair <hossam.khir1@outlook.com>
2020-12-11 17:14:15 +0200

    removed redundant file

--- quiz.R
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/Rscript
-
-source("corr.R")
-
-cr <- corr("specdata", 2000)                
-n <- length(cr)                
-cr <- corr("specdata", 1000)                
-cr <- sort(cr)
-print(c(n, round(cr, 4)))
